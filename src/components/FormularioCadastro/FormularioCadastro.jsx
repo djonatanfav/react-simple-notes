d4fc1f3a7e665bbf054859fc3f7281f487ad7d4d
Djonatan Fávero <djonatanfavero@MacBook-Air-de-Djonatan.local>
2021-09-07 19:03:32 -0300

    Final do curso

--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -3,19 +3,32 @@ import "./estilo.css";
 
 class FormularioCadastro extends Component {
 
-  constructor(){
-    super();
+  constructor(props){
+    super(props);
     this.titulo = "";
+    this.texto  = "";
   }
 
   handleMudancaTitulo(evento){
     this.titulo = evento.target.value;
-    console.log(this.titulo);
+  }
+
+  handleMudancaTexto(evento){
+    this.texto = evento.target.value;
+  }
+
+  criarNota(evento){
+    evento.preventDefault();
+    evento.stopPropagation();
+    
+    this.props.criarNota(this.titulo, this.texto)
   }
   
   render() {
     return (
-      <form className="form-cadastro ">
+      <form className="form-cadastro "
+        onSubmit={this.criarNota.bind(this)}
+      >
         <input
           type="text"
           placeholder="Título"
@@ -26,6 +39,7 @@ class FormularioCadastro extends Component {
           rows={15}
           placeholder="Escreva sua nota..."
           className="form-cadastro_input"
+          onChange={this.handleMudancaTexto.bind(this)}
         />
         <button className="form-cadastro_input form-cadastro_submit">
           Criar Nota
